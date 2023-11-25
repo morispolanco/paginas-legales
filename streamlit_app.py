55bb473db4490c0432e8b1c2d0e9f9deca3288a1
morispolanco <morispolanco@gmail.com>
2023-11-24 22:01:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,34 +8,34 @@ def home_page():
 def page_one():
     st.title("Leyes")
     st.write("import streamlit as st
-import requests
-
-def get_api_response(question):
-    url = "https://api.afforai.com/api/api_completion"
-    payload = {
-        "apiKey": "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e",
-        "sessionID": "65489d7c9ad727940f2ab26f",
-        "history": [
-            {
-                "role": "user",
-                "content": question
-            }
-        ],
-        "powerful": True,
-        "google": True
-    }
-    response = requests.post(url, json=payload)
-    return response.json()
-
-def main():
-    st.title("Preguntas sobre las leyes de Guatemala")
-    question = st.text_input("Escribe tu pregunta")
-    if st.button("Enviar"):
-        response = get_api_response(question)
-        st.write(response)
-
-if __name__ == "__main__":
-    main()")
+    import requests
+    
+    def get_api_response(question):
+        url = "https://api.afforai.com/api/api_completion"
+        payload = {
+            "apiKey": "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e",
+            "sessionID": "65489d7c9ad727940f2ab26f",
+            "history": [
+                {
+                    "role": "user",
+                    "content": question
+                }
+            ],
+            "powerful": True,
+            "google": True
+        }
+        response = requests.post(url, json=payload)
+        return response.json()
+    
+    def main():
+        st.title("Preguntas sobre las leyes de Guatemala")
+        question = st.text_input("Escribe tu pregunta")
+        if st.button("Enviar"):
+            response = get_api_response(question)
+            st.write(response)
+    
+    if __name__ == "__main__":
+        main()")
 
 def page_two():
     st.title("Página 2")
