1de37bf5d2bf1c80422e8f7c1cfd4e682309ec46
morispolanco <morispolanco@gmail.com>
2023-11-25 03:27:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,14 @@ import streamlit as st
 import requests
 import json
 
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
 
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 # Funciones para cada página
 
