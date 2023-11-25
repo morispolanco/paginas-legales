614683998df43b90bd7ac06c3159a06a7c49fb8a
morispolanco <morispolanco@gmail.com>
2023-11-24 21:58:58 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,36 @@ def home_page():
     st.write("¡Bienvenido a la Página de Inicio!")
 
 def page_one():
-    st.title("Página 1")
-    st.write("Contenido de la Página 1.")
+    st.title("Leyes")
+    st.write("import streamlit as st
+import requests
+
+def get_api_response(question):
+    url = "https://api.afforai.com/api/api_completion"
+    payload = {
+        "apiKey": "fcbfdfe8-e9ed-41f3-a7d8-b6587538e84e",
+        "sessionID": "65489d7c9ad727940f2ab26f",
+        "history": [
+            {
+                "role": "user",
+                "content": question
+            }
+        ],
+        "powerful": True,
+        "google": True
+    }
+    response = requests.post(url, json=payload)
+    return response.json()
+
+def main():
+    st.title("Preguntas sobre las leyes de Guatemala")
+    question = st.text_input("Escribe tu pregunta")
+    if st.button("Enviar"):
+        response = get_api_response(question)
+        st.write(response)
+
+if __name__ == "__main__":
+    main()")
 
 def page_two():
     st.title("Página 2")
